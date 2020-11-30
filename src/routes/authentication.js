9b2807d97f6b5fde53f1f8d70c068c308b177b14
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2020-11-30 17:39:38 -0300

    data

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,69 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const passport = require('passport');
-const { isLoggedIn } = require('../lib/auth');
-const passportconfig = require('../lib/passportconfig');
+const passport = require("passport");
+const { isLoggedIn } = require("../lib/auth");
+const passportconfig = require("../lib/passportconfig");
 passportconfig(passport);
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local-login',(err, user, info) => {
-        if (err) {
-            res.send({
-                code: 400,
-                failed: "un error ha ocurrido",
-            });
-        };
-        if (!user){
-            res.send({
-                code: 206,
-                success: "Usuario no existe",
-            });
-        }
-        else {
-            req.logIn(user, (err) => {
-                if (err) throw err;
-                res.send({
-                    code: 200,
-                    success: "inicio de sesión exitoso",
-                });
-                console.log(req.user);
-            });
-        }
-    })(req, res, next);
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local-login", (err, user, info) => {
+    if (err) {
+      res.send({
+        code: 400,
+        failed: "un error ha ocurrido",
+      });
+    }
+    if (!user) {
+      res.send({
+        code: 206,
+        success: "Usuario no existe",
+      });
+    } else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send({
+          code: 200,
+          success: "inicio de sesión exitoso",
+        });
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
 });
 
-
-router.post('/signup', (req, res, next) => {
-    passport.authenticate('local-signup',(err, user, info) => {
-        if (err) {
-            res.send({
-                code: 400,
-                failed: "un error ha ocurrido",
-            });
-        };
-        if (!user){
-            res.send({
-                code: 206,
-                success: "El email usado ya esta tomado",
-            });
-        }
-        else {
-            req.logIn(user, (err) => {
-                if (err) throw err;
-                res.send({
-                    code: 200,
-                    success: "Registro nuevo exitoso",
-                });
-                console.log(req.user);
-            });
-        }
-    })(req, res, next);
+router.post("/signup", (req, res, next) => {
+  passport.authenticate("local-signup", (err, user, info) => {
+    if (err) {
+      res.send({
+        code: 400,
+        failed: "un error ha ocurrido",
+      });
+    }
+    if (!user) {
+      res.send({
+        code: 206,
+        success: "El email usado ya esta tomado",
+      });
+    } else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send({
+          code: 200,
+          success: "Registro nuevo exitoso",
+        });
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
 });
 
-router.get('/logout', (req, res) => {
-    req.logOut();
-    res.send('LogOut exitoso');
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.send("LogOut exitoso");
 });
 
 module.exports = router;
